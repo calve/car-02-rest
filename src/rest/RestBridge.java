45a49b2690a34e3590a8f8ac35aa7919b7870e34
calve <calvinh34@gmail.com>
2015-02-24 18:01:30 +0100

    Comments

--- src/rest/RestBridge.java
+++ src/rest/RestBridge.java
@@ -1,5 +1,8 @@
 package rest;
 
+/**
+ * Front end classes. Listen to incoming connexion, dispatch them to the FTP client
+ */
 public class RestBridge {
     public static final int LISTENING_PORT = 8080;
 
@@ -7,5 +10,5 @@ public class RestBridge {
         System.out.println("I am a rest bridge, which unfortunately does nothing yet");
         System.out.println("Bye");
     }
- 
+
 }
