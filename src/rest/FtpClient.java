691f0f213a4ee77ea93c6ac1d90962d8c108697a
calve <calvinh34@gmail.com>
2015-03-10 18:25:44 +0100

    Wire FtpClient to an FtpServer

--- src/rest/FtpClient.java
+++ src/rest/FtpClient.java
@@ -1,5 +1,8 @@
 package rest;
 
+import java.io.IOException;
+import java.net.SocketException;
+
 public class FtpClient {
     /** Connects to the FTP serveur located at ip:port,
      * using (user, password) as credentials
@@ -8,8 +11,25 @@ public class FtpClient {
     private String ip;
     private int port;
     private String user;
+    private org.apache.commons.net.ftp.FTPClient ftp;
 
     public FtpClient(String ip, int port, String user, String password){
+        this.ftp = new org.apache.commons.net.ftp.FTPClient();
+        try {
+            this.ftp.connect(ip, port);
+        }
+        catch (SocketException e){
+            System.out.println("Failed to connect to ftp server");
+        }
+        catch (IOException e){
+            System.out.println("Failed to log to ftp server");
+        }
+        try {
+            this.ftp.login(user, password);
+        }
+        catch (IOException e){
+            System.out.println("Failed to log to ftp server");
+        }
     }
 
     /* Retrieve the content of ``path``
