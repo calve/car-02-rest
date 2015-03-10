bfe985ab01513a97397a6a4043273716ae32c799
calve <calvinh34@gmail.com>
2015-03-10 18:03:25 +0100

    Wire RestBrigde to FtpClient

--- src/rest/RestBridge.java
+++ src/rest/RestBridge.java
@@ -2,40 +2,85 @@ package rest;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Scanner;
 import java.net.InetSocketAddress;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
+import rest.FtpClient;
+
 /**
  * Front end classes. Listen to incoming connexion, dispatch them to the FTP client
  */
 public class RestBridge {
     public static final int LISTENING_PORT = 8080;
+    private String ip;
+    private int port;
+    private String user;
+    private String password;
 
-    public static void main(String[] args) throws Exception {
-        System.out.println("I am a rest bridge, which unfortunately does nothing yet");
+    public RestBridge(String ip, int port, String user) throws Exception {
+        this.ip = ip;
+        this.port = port;
+        this.user = user;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Password ? ");
+        password = scanner.next();
+        this.password = password;
+        System.out.println();
         System.out.println("Listening on "+LISTENING_PORT);
         HttpServer server = HttpServer.create(new InetSocketAddress(LISTENING_PORT), 0);
-        server.createContext("/", new FtpHandler());
+        server.createContext("/", new FtpHandler(this.ip, this.port, this.user, this.password));
         server.setExecutor(null);
         server.start();
+    }
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 3) {
+            System.out.println("Usage : restclient ip port username");
+            return;
+        }
+        RestBridge restBrigde = new RestBridge(args[0], Integer.parseInt(args[1]), args[2]);
     }
 
     /**
      * Handles one HTTP requests
      */
     static class FtpHandler implements HttpHandler {
+        private String ip;
+        private int port;
+        private String user;
+        private String password;
+
+        public FtpHandler(String ip, int port, String user, String password){
+            this.ip = ip;
+            this.port = port;
+            this.user = user;
+            this.password = password;
+        }
 
         /* Handles one HttpExchange.
-         * Should instanciate a FtpClient using the RequestURI as parameters,
+         * Should instanciate a FtpClient using the RequestURI as action,
          * and output the response of that FtpClient
          */
         public void handle(HttpExchange t) throws IOException {
-            String response = "This is the response for "+t.getRequestURI();
+            String httpVerb = t.getRequestMethod();
+            String uri = ""+t.getRequestURI(); /* Bizarre : fails to compile without the empty string */
+            FtpClient ftpClient = new FtpClient(this.ip, this.port, this.user, this.password);
+
+            String response = String.format("This is the response for %s (%s)", uri, httpVerb);
+            switch (httpVerb){
+            case "GET":
+                response = ftpClient.get(uri).toString();
+                break;
+            case "POST":
+                response = ftpClient.post(uri, "".getBytes()).toString();
+                break;
+            }
             /* Log this request on the server */
-            System.out.println(t.getRequestMethod() + ": "+t.getRequestURI());
+            System.out.println(httpVerb + ": "+uri);
             t.sendResponseHeaders(200, response.length());
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
