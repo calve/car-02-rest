a8a0f03e1745ffaf90acef72194071eb068cd19b
calve <calvinh34@gmail.com>
2015-03-10 17:27:00 +0100

    FtpClient skeleton

---
+++ src/rest/FtpClient.java
@@ -0,0 +1,24 @@
+public class FtpClient {
+    /** Connects to the FTP serveur located at ip:port,
+     * using (user, password) as credentials
+     */
+
+    private String ip;
+    private int port;
+    private String user;
+
+    public FtpClient(String ip, int port, String user, String password){
+    }
+
+    /* Retrieve the content of ``path``
+     */
+    byte[] get(String path){
+        return "".getBytes();
+    }
+
+    /* Post ``data`` at ``path``
+     */
+    byte[] post(String path, byte data){
+        return "".getBytes();
+    }
+}
