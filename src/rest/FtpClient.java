ac80696b7cf08591db23b8d7071eaa8113ae1805
calve <calvinh34@gmail.com>
2015-03-10 18:42:31 +0100

    List a directory on GET

--- src/rest/FtpClient.java
+++ src/rest/FtpClient.java
@@ -3,6 +3,8 @@ package rest;
 import java.io.IOException;
 import java.net.SocketException;
 
+import org.apache.commons.net.ftp.FTPFile;
+
 public class FtpClient {
     /** Connects to the FTP serveur located at ip:port,
      * using (user, password) as credentials
@@ -11,7 +13,7 @@ public class FtpClient {
     private String ip;
     private int port;
     private String user;
-    private org.apache.commons.net.ftp.FTPClient ftp;
+    private org.apache.commons.net.ftp.FTPClient ftp; /* We do not import it to not mess with our own class name */
 
     public FtpClient(String ip, int port, String user, String password){
         this.ftp = new org.apache.commons.net.ftp.FTPClient();
@@ -30,11 +32,26 @@ public class FtpClient {
         catch (IOException e){
             System.out.println("Failed to log to ftp server");
         }
+        System.out.println("Successfully logged in");
     }
 
     /* Retrieve the content of ``path``
      */
-    byte[] get(String path){
+    byte[] get(String path) throws IOException{
+        System.out.println(path);
+        if (path.substring(path.length() - 1).charAt(0) == '/'){
+            /* List the directory ! */
+            FTPFile[] files = this.ftp.listFiles(path);
+            String output = "";
+            for (FTPFile file : files) {
+                output += file.getName()+"\n";
+            }
+            return output.getBytes();
+        }
+        else {
+            System.out.println("not a dir");
+            /* Here we should RECV */
+        }
         return "".getBytes();
     }
 
